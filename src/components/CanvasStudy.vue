dbac0aca3373d2298defdb46cf1bf3d1bb10b3e2
weichaozhan <921391554@qq.com>
2018-08-30 20:45:31 +0800

    canvas

--- src/components/CanvasStudy.vue
+++ src/components/CanvasStudy.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    canvas
+    <canvas id ="canvas" width="500" height="500">
+    </canvas>
   </div>
 </template>
 
@@ -12,7 +13,32 @@ export default {
     }
   },
   components: {
+  },
+  mounted() {
+    let canvas = document.getElementById('canvas')
+    let context = canvas.getContext('2d')
 
+    context.fillStyle = '#ffffaa'
+    context.fillRect(0, 0, 500, 500)
+
+    context.fillStyle = '#000000'
+    context.font = '20px Sans-Serif'
+    context.textBalseline = 'top'
+    context.fillText('Hello World!', 195, 80)
+
+    // 图片
+    let helloWorldImage = new Image(100, 100)
+
+    helloWorldImage.src = require('../assets/timg.jpg')
+
+    helloWorldImage.addEventListener('load', function() {
+      context.drawImage(helloWorldImage, 160, 130, 100, 100)
+    })
+
+    context.strokeStyle = '#000000'
+    context.strokeRect(5, 5, 490, 290)
+
+    console.log(canvas.toDataURL())
   }
 }
 </script>
